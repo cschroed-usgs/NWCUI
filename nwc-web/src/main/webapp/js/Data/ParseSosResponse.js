68f18d487545af1fde2f6b9030860e4cd514ce3c
Carl Schroedl <cschroedl@usgs.gov>
2013-09-13 13:33:52 -0500

    Replacing old sos parsing function with new one

--- nwc-web/src/main/webapp/js/Data/ParseSosResponse.js
+++ nwc-web/src/main/webapp/js/Data/ParseSosResponse.js
@@ -1,33 +1,38 @@
 Ext.ns('NWCUI.data');
-NWCUI.data.parseSosResponse = function(responseTxt, numFieldsLoadedLater){
-    responseTxt = responseTxt.slice(0, responseTxt.length-2);//kill terminal ' \n'
-    var rows = responseTxt.split(' ');
-    var rightPadding = [];
-    for(var i = 0; i < numFieldsLoadedLater; i++){
-        rightPadding.push(null);
-    }
-    var finalRows = [];
-    var nonNanHasBeenFound = false;
-    rows.each(function(row){
-        var tokens = row.split(',');
+/**
+ * @param {object} response
+ * @returns {object} - an object with data and metadata fields
+ */
+NWCUI.data.parseSosResponse = function(response){
+    var parser = new OpenLayers.Format.SOSGetObservation();
+    var parsedSOS = parser.read(response);
+    var parsedObject = {
+        metadata: {
+          seriesName : parsedSOS.observations[0].result.dataArray.dataRecord[1].name,
+          seriesUnits : parsedSOS.observations[0].result.dataArray.dataRecord[1].uom
+        },
+        data: parsedSOS.observations[0].result.dataArray.values
+    };
 
-        var dateStr = tokens[0].to(tokens[0].indexOf('T'));
-        dateStr = dateStr.replace(/-/g,'/');
-        var date = new Date(dateStr);
-        var value = parseFloat(tokens[1]);
-        if(NWCUI.data.parseSosResponse.emptyValueThreshold <= value){
-            value = NaN;
-        }
-        //Do not display leading NaNs in periods of record.
-        //In other words:
-        //Only add the parsed row to final rows if the current value is a number
-        //or if the current value is NaN, but a previously-parsed value was a number
+    //remove whitespace,
+    //for date field, convert from ISO to regular date to avoid 
+    //artificial timezone correction
+    //for numerical value, parseFloat and convert 'missing' values to NaN
+    var missingValues = [9.96921e+36, -999];
+    parsedObject.data = parsedObject.data.map(function(datum){
+        var dateStr = datum[0],
+            numericalValue = parseFloat(datum[1]);
 
-        if(!isNaN(value) || (isNaN(value) && nonNanHasBeenFound)){//could be optimized to use implicit logic, but this way is more intelligible
-            finalRows.push([date, value].concat(rightPadding));
-            nonNanHasBeenFound = true;  //it is a number, 
+        if(missingValues.any(numericalValue)){
+            numericalValue = NaN;
         }
+        dateStr = dateStr.trim();
+        var timeIndex = dateStr.indexOf('T');//ISO time marker
+        dateStr = dateStr.slice(0, timeIndex);
+        dateStr = dateStr.replace(/-/g, '/');
+
+        return [dateStr, numericalValue];
     });
-    return finalRows;
+    return parsedObject;
 };
 NWCUI.data.parseSosResponse.emptyValueThreshold = 9.96921e+36;//any precip values above this amount will be considered NaN's
--- nwc-web/src/main/webapp/js/Map/map.js
+++ nwc-web/src/main/webapp/js/Map/map.js
@@ -393,42 +393,6 @@ NWCUI.MapPanel = Ext.extend(GeoExt.MapPanel, {
                 });
             }
         },
-    /**
-     * @param {object} response
-     * @returns {object} - an object with data and metadata fields
-     */
-    parseSosResponse : function(response){
-        var parser = new OpenLayers.Format.SOSGetObservation();
-        var parsedSOS = parser.read(response);
-        var parsedObject = {
-            metadata: {
-              seriesName : parsedSOS.observations[0].result.dataArray.dataRecord[1].name,
-              seriesUnits : parsedSOS.observations[0].result.dataArray.dataRecord[1].uom
-            },
-            data: parsedSOS.observations[0].result.dataArray.values
-        };
-        
-        //remove whitespace,
-        //for date field, convert from ISO to regular date to avoid 
-        //artificial timezone correction
-        //for numerical value, parseFloat and convert 'missing' values to NaN
-        var missingValues = [9.96921e+36, -999];
-        parsedObject.data = parsedObject.data.map(function(datum){
-            var dateStr = datum[0],
-                numericalValue = parseFloat(datum[1]);
-            
-            if(missingValues.any(numericalValue)){
-                numericalValue = NaN;
-            }
-            dateStr = dateStr.trim();
-            var timeIndex = dateStr.indexOf('T');//ISO time marker
-            dateStr = dateStr.slice(0, timeIndex);
-            dateStr = dateStr.replace(/-/g, '/');
-            
-            return [dateStr, numericalValue];
-        });
-        return parsedObject;
-    },
     buildSosUrl: function(offering, observedProperty, dataset, fileName){
         var sosParams = {
             request: 'GetObservation',
