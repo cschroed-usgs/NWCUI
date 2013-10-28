3c3174b8aa120671012debcfc2f93458c05fee76
Carl Schroedl <cschroedl@usgs.gov>
2013-10-28 15:50:48 -0500

    Refactored existing tests to eliminate verbose test data hardcoding and let us focus on the logic. Added tests for daily water use update. TODO: tests for monthly water use update.

--- nwc-web/src/test/javascript/specs/DataSeriesStore_spec.js
+++ nwc-web/src/test/javascript/specs/DataSeriesStore_spec.js
@@ -1,44 +1,27 @@
 initializeLogging();
 describe('DataSeriesStore', function(){
-    var daymetData = [
-        ['1951/01/01', 1.0],
-        ['1951/01/02', 1.0],
-        ['1951/01/03', 1.0],
-        ['1951/01/04', 1.0],
-        ['1951/01/05', 1.0],
-        ['1951/01/06', 1.0],
-        ['1951/01/07', 1.0],
-        ['1951/01/08', 1.0],
-        ['1951/01/09', 1.0],
-        ['1951/01/10', 1.0],
-        ['1951/01/11', 1.0],
-        ['1951/01/12', 1.0],
-        ['1951/01/13', 1.0],
-        ['1951/01/14', 1.0],
-        ['1951/01/15', 1.0],
-        ['1951/01/16', 1.0],
-        ['1951/01/17', 1.0],
-        ['1951/01/18', 1.0],
-        ['1951/01/19', 1.0],
-        ['1951/01/20', 1.0],
-        ['1951/01/21', 1.0],
-        ['1951/01/22', 1.0],
-        ['1951/01/23', 1.0],
-        ['1951/01/24', 1.0],
-        ['1951/01/25', 1.0],
-        ['1951/01/26', 1.0],
-        ['1951/01/27', 1.0],
-        ['1951/01/28', 1.0],
-        ['1951/01/29', 1.0],
-        ['1951/01/30', 1.0],
-        ['1951/01/31', 1.0]
-    ];
+    var dateRangeStart = Date.create('March 1951');
+    var dateRangeEnd = Date.create('June 1951').endOfMonth().beginningOfDay();
+    var dateRange = Date.range(
+        dateRangeStart,
+        dateRangeEnd
+    );
+    var formatDate = function(date){
+        return date.format('{yyyy}/{MM}/{dd}');
+    };
+    
+    var dayMetData = [];
+    dateRange.every('day', function(date){
+        var dateStr = formatDate(date);
+       dayMetData.push([dateStr, 1.0]);
+    });
+    
     var dayMetDataSeries = NWCUI.data.DataSeries();
-    dayMetDataSeries.data = daymetData;
+    dayMetDataSeries.data = dayMetData;
     dayMetDataSeries.metadata.seriesLabels.push('mm ppt');
 
     var etaData = [
-        ['1951/01/01', 1.0]
+        [formatDate(dateRangeStart), 1.0]
     ];
     var etaDataSeries = NWCUI.data.DataSeries();
     etaDataSeries.data = etaData;
@@ -55,10 +38,12 @@ describe('DataSeriesStore', function(){
     beforeEach(function (){
         dss = dss;
         dataSeries=dataSeries;
+        dateRangeStart = dateRangeStart;
+        dateRangeEnd = dateRangeEnd;
     });    
     describe('DataSeriesStore#updateMonthlySeries', function(){
         it('should handle the case that the period of record ends on the last day of a month', function(){
-            expect(dss.monthly.data.length).toBe(1);
+            expect(dss.monthly.data.length).toBe(4);
         });
         it('should correctly acummulate eta and join it to monthly eta', function(){
             expect(dss.monthly.data[0][1]).toBe(31);
@@ -70,6 +55,62 @@ describe('DataSeriesStore', function(){
             incompleteDaymetDataSeries.dayMet.data = incompleteDaymetDataSeries.dayMet.data.to(incompleteDaymetDataSeries.dayMet.data.length-1);
             var incompleteDss = new NWCUI.data.DataSeriesStore(incompleteDaymetDataSeries);
             expect(incompleteDss.monthly.data.length).toBe(0);
+        });
+    });
+    describe('DataSeriesStore#updateWaterUseSeries', function(){
+        //subsequent tests presume 3 rows on monthly increments, starting with
+        //the first month of the eta time series and ending with the first day of 
+        //the last month of the eta time series
+       
+       var mockWaterUseData = [
+           [dateRangeStart.clone().addMonths(1), 1, 2, 3],
+           [dateRangeStart.clone().addMonths(2), NaN, NaN, NaN],
+           [dateRangeStart.clone().addMonths(3), 4, 5, 6]
+       ];
+       var labels = ['Date', 'Drinking', 'Industry', 'Irrigation'];
+       var mockWaterUseSeries = new NWCUI.data.DataSeries();
+       mockWaterUseSeries.data = mockWaterUseData;
+       mockWaterUseSeries.metadata.seriesLabels = labels;
+       dss.updateWaterUseSeries(mockWaterUseSeries);
+       var getWaterUseValuesFromMergedRow = function(row){
+         return row.from(3);
+       };
+       var getWaterUseValuesFromMockRow = function(row){
+           return row.from(1);
+       }
+       it('should expand the target time series to the correct dimensions',function(){
+            //ensure the correct dims were achieved
+            expect(dss.daily.data[0].length).toBe(6);
+       });
+       it('should not join the water use values into the existing time series early', function(){
+           //find index of last day of first month
+           var mergedIndex = dateRangeStart.daysInMonth() - 1;//subtract because 0-based
+           var waterUseValuesFromMergedRow = getWaterUseValuesFromMergedRow(dss.daily.data[mergedIndex]);
+           var waterUseValuesFromMockRow = getWaterUseValuesFromMockRow(mockWaterUseData[0]);
+           expect(waterUseValuesFromMergedRow).not.toEqual(waterUseValuesFromMockRow);
+       });
+       it('should join the water use values into the existing time series starting at the appropriate time step', function(){
+           var mergedIndex = dateRangeStart.daysInMonth();//actually indexes into next month because it's 0-based
+           var waterUseValuesFromMergedRow = getWaterUseValuesFromMergedRow(dss.daily.data[mergedIndex]);
+           var waterUseValuesFromMockRow = getWaterUseValuesFromMockRow(mockWaterUseData[0]);
+           expect(waterUseValuesFromMergedRow).toEqual(waterUseValuesFromMockRow);
+       });
+        it('should switch which water use values are joined onto the time series when the next water use timestep has been reached', function () {
+            var mergedIndex = dateRangeStart.daysInMonth();
+            mergedIndex += dateRangeStart.clone().addMonths(1).daysInMonth();
+            var waterUseValuesFromMergedRow = getWaterUseValuesFromMergedRow(dss.daily.data[mergedIndex]);
+            var waterUseValuesFromMockRow = getWaterUseValuesFromMockRow(mockWaterUseData[1]);
+            //entreating special NaN case RE: NaN != NaN in JS
+            var waterUseValuesFromMergedRowAreNaNs = waterUseValuesFromMergedRow.map(isNaN);
+            var waterUseValuesFromMockRowAreNaNs = waterUseValuesFromMockRow.map(isNaN);
+            expect(waterUseValuesFromMergedRowAreNaNs).toEqual(waterUseValuesFromMockRowAreNaNs);
+
+            //try the next Timestep too, it has non-NaNs
+            mergedIndex += dateRangeStart.clone().addMonths(2).daysInMonth();
+            var waterUseValuesFromMergedRow = getWaterUseValuesFromMergedRow(dss.daily.data[mergedIndex]);
+            var waterUseValuesFromMockRow = getWaterUseValuesFromMockRow(mockWaterUseData[2]);
+            expect(waterUseValuesFromMergedRow).toEqual(waterUseValuesFromMockRow);
+
         });
     });
 });
