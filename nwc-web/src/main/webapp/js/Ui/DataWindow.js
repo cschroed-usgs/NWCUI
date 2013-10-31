48b3613e0fd0e5e127a6cdf48ed7d37461c7fa60
Carl Schroedl <cschroedl@usgs.gov>
2013-10-31 12:17:29 -0500

    JSLinting Ui files.

--- nwc-web/src/main/webapp/js/Ui/DataWindow.js
+++ nwc-web/src/main/webapp/js/Ui/DataWindow.js
@@ -1,18 +1,22 @@
+/*global Ext,LOG,CONFIG,NWCUI,$,GeoExt,OpenLayers,gxp*/
 Ext.ns("NWCUI.ui");
 
 NWCUI.ui.DataWindow = Ext.extend(Ext.Window, {
-    constructor: function(config) {
+    constructor: function (config) {
         var self = this;
         var title = config.title || "";
         var width = config.width || 1000;
         var height = config.height || 450;
         self.defaultSeries = 'monthly';
-        self.dataSeriesStore = config.dataSeriesStore || {};
-        self.toggleBar = new NWCUI.ui.SeriesToggleToolbar({window: self});
-               //attach the contained components so that they can be easily referenced later
+        self.dataSeriesStore = config.dataSeriesStore || {
+        };
+        self.toggleBar = new NWCUI.ui.SeriesToggleToolbar({
+            window: self
+        });
+        //attach the contained components so that they can be easily referenced later
         self.graphPanel = new NWCUI.ui.StatsGraphPanel();
         self.labelPanel = new NWCUI.ui.StatsLabelPanel();
-        var updateGraph = function(window){
+        var updateGraph = function (window) {
             window = window || self;
             var graphDiv = window.graphPanel.getEl().dom;
             var legendDiv = window.labelPanel.getEl().dom;
@@ -22,29 +26,32 @@ NWCUI.ui.DataWindow = Ext.extend(Ext.Window, {
             window.doLayout();
             self.graphPanel.graph = graph;
         };
-        config = Ext.apply({
-            width: width,
-            height: height,
-            tbar: self.toggleBar,
-            title: title,
-            collapsible: true,
-            layout : 'hbox',
-            items: [self.graphPanel, self.labelPanel],
-            bbar: new NWCUI.ui.DataExportToolbar(),
-            listeners:{
-                afterrender: function(window){
-                    updateGraph(window);
-                },
-                collapse: function(window){
-                    window.alignTo(Ext.get('nwcui-body-panel'), 'tl', [50, 0], true);
-                },
-                expand: function (window) {
-                    window.center();
+        config = Ext.apply(
+            {
+                width: width,
+                height: height,
+                tbar: self.toggleBar,
+                title: title,
+                collapsible: true,
+                layout: 'hbox',
+                items: [self.graphPanel, self.labelPanel],
+                bbar: new NWCUI.ui.DataExportToolbar(),
+                listeners: {
+                    afterrender: function (window) {
+                        updateGraph(window);
+                    },
+                    collapse: function (window) {
+                        window.alignTo(Ext.get('nwcui-body-panel'), 'tl', [50, 0], true);
+                    },
+                    expand: function (window) {
+                        window.center();
 
-                }
+                    }
+                },
+                updateGraph: updateGraph
             },
-            updateGraph: updateGraph
-        }, config);
+            config
+        );
 
         NWCUI.ui.DataWindow.superclass.constructor.call(this, config);
         LOG.info('NWCUI.ui.DataWindow::constructor(): Construction complete.');
