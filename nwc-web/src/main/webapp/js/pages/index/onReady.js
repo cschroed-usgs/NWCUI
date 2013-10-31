2dd5b4b01584f805c65338b34d8b5051a20bcf9d
Carl Schroedl <cschroedl@usgs.gov>
2013-10-31 15:00:09 -0500

    JSLinting onReady.js

--- nwc-web/src/main/webapp/js/pages/index/onReady.js
+++ nwc-web/src/main/webapp/js/pages/index/onReady.js
@@ -1,8 +1,9 @@
+/*global Ext,LOG,CONFIG,NWCUI */
 if (Ext.isIE) { // http://www.mail-archive.com/users@openlayers.org/msg01838.html
     document.namespaces;
 }
 
-Ext.onReady(function() {
+Ext.onReady(function () {
     initializeLogging({
         LOG4JS_LOG_THRESHOLD: CONFIG.LOG4JS_LOG_THRESHOLD
     });
@@ -58,58 +59,64 @@ Ext.onReady(function() {
             footerPanel
         ]
     });
-    
+
 });
 
-function initializeAjax() {
+var initializeAjax = function () {
 
     Ext.Ajax.addEvents(
-            "ajax-request-firing",
-            "ajax-requests-complete",
-            "ajax-request-exception"
-            );
+        "ajax-request-firing",
+        "ajax-requests-complete",
+        "ajax-request-exception"
+    );
 
-    Ext.Ajax.on('beforerequest', function(connection, options) {
+    Ext.Ajax.on('beforerequest', function (connection, options) {
         document.body.style.cursor = 'wait';
-        CONFIG.mapPanel.map.div.style.cursor = 'wait'
+        CONFIG.mapPanel.map.div.style.cursor = 'wait';
         if (!Ext.Ajax.isLoading()) {
-            Ext.Ajax.fireEvent('ajax-request-firing',
-                    {
-                        connection: connection,
-                        options: options
-                    });
+            Ext.Ajax.fireEvent(
+                'ajax-request-firing',
+                {
+                    connection: connection,
+                    options: options
+                }
+            );
         }
     }, this);
 
-    Ext.Ajax.on('requestcomplete', function(connection, response, options) {
+    Ext.Ajax.on('requestcomplete', function (connection, response, options) {
         document.body.style.cursor = 'default';
-        CONFIG.mapPanel.map.div.style.cursor = 'default'
+        CONFIG.mapPanel.map.div.style.cursor = 'default';
         if (!Ext.Ajax.isLoading()) {
-            Ext.Ajax.fireEvent('ajax-requests-complete',
-                    {
-                        connection: connection,
-                        response: response,
-                        options: options
-                    });
+            Ext.Ajax.fireEvent(
+                'ajax-requests-complete',
+                {
+                    connection: connection,
+                    response: response,
+                    options: options
+                }
+            );
         }
     }, this);
 
-    Ext.Ajax.on('requestexception', function(connection, response, options) {
+    Ext.Ajax.on('requestexception', function (connection, response, options) {
         LOG.error(response);
         document.body.style.cursor = 'default';
-        CONFIG.mapPanel.map.div.style.cursor = 'default'
+        CONFIG.mapPanel.map.div.style.cursor = 'default';
         if (!Ext.Ajax.isLoading()) {
-            Ext.Ajax.fireEvent('ajax-request-exception',
-                    {
-                        connection: connection,
-                        response: response,
-                        options: options
-                    });
+            Ext.Ajax.fireEvent(
+                'ajax-request-exception',
+                {
+                    connection: connection,
+                    response: response,
+                    options: options
+                }
+            );
         }
     }, this);
-}
+};
 
-function initializeQuickTips() {
+var initializeQuickTips = function () {
     Ext.QuickTips.init();
 
     Ext.apply(Ext.QuickTips.getQuickTip(), {
@@ -119,4 +126,4 @@ function initializeQuickTips() {
         dismissDelay: 0,
         trackMouse: true
     });
-}
+};
